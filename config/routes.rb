00a837ed63d20e07cc423a0d3d639952c2a637a0
PatriciaChebet <patriciacchebet@gmail.com>
2022-10-16 15:37:37 +0300

    Shows specific destination details

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   get 'tags' => 'tags#index'
   get '/tags/:id' => 'tags#show', as: :tag
+  get '/destinations/:id' => 'destinations#show', as: :destination
 end
